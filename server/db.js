a14c39025f6bf460086be47f316ef5cc96dce7ca
Whitneyyx00 <whitneyyx77@gmail.com>
2025-05-07 22:31:43 -0400

    Set up MongoDB database

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:<your-password>@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:HguFgDjXyfHqVFkFS0GP9oRc@172.21.195.240:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
